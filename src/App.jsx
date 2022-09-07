1376161ba179dfd286e6de993bcc08e7c3ad491a
DmytroMartus <dimesiys@gmail.com>
2022-09-07 17:51:49 +0300

    Removed brackets from app and about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import React from 'react';
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
-import About from './pages/About';
+import About from "./pages/About";
 
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 import "./app.scss";
 
-const App = () => {
-  return (
-    <>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-      <Footer/>
-    </>
-  );
-};
+const App = () => (
+  <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+    <Footer />
+  </>
+);
 
 export default App;
